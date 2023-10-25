17bd8518098677fda352741b4e92c100aea0e19c
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2023-10-25 17:18:26 +1100

    Initial Commit

---
+++ StateTest1_LearnPeppe/StateTest1_LearnPeppeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateTest1_LearnPeppeApp.swift
+//  StateTest1_LearnPeppe
+//
+//  Created by Giuseppe Sapienza on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct StateTest1_LearnPeppeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
